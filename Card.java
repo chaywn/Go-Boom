de8be4143083aec7e28f8136b8649d61b4da1d06
1221302637 <123657438+CheeChoong14@users.noreply.github.com>
2023-05-08 23:19:45 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,43 +1,35 @@
-public class Card 
-    implements Comparable<Card> {
-    public static final char[] SUITS = {'c', 'd', 'h', 's'};
-    public static final char[] RANKS = {'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A'};
-
+public class Card {
+    public static final char[] SUITS = {'H', 'D', 'C', 'S'};
+    public static final char[] RANKS = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
+    
     private char suit;
     private char rank;
- 
+    
     public Card(char suit, char rank) {
         this.suit = suit;
         this.rank = rank;
     }
-
+    
     public char getSuit() {
         return suit;
     }
-
+    
     public char getRank() {
         return rank;
     }
 
+    public int compareTo(Card otherCard) {
+        if (this.rank > otherCard.rank) {
+            return 1;
+        } else if (this.rank < otherCard.rank) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+    
     @Override
     public String toString() {
-        return "" + suit + rank ;
-    }
-
-    @Override
-    public int compareTo(Card c) {
-        int index = 0, c_index = 0;
-
-        for (int i = 0; i < RANKS.length; i++) {
-            if (RANKS[i] == rank) {
-                index = i;
-            }
-
-            if (RANKS[i] == c.rank) {
-                c_index = i;
-            }
-        }
-
-        return index - c_index;
+        return "" + suit + rank;
     }
 }
