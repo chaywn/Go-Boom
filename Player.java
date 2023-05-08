5f989fef02adfb43278ef0b3950fa6e20998bec2
1221302637 <123657438+CheeChoong14@users.noreply.github.com>
2023-05-08 23:20:22 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,34 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Player {
     private int number;
+    private ArrayList<Card> cards;
     private int score;
     private Deck deck;
+    private Card leadCard;
 
     public Player(int number) {
         this.number = number;
+        this.cards = new ArrayList<Card>();
+        this.score = 0;
     }
 
     public int getNumber() {
-        return number;
+        return this.number;
     }
 
     public int getScore() {
-        return score;
+        return this.score;
     }
 
-    public void retrieveCards(int noOfCards, Deck mainDeck) {
-        if (Objects.isNull(deck)) {
-            deck = new Deck(noOfCards, mainDeck);
+    public void retrieveCards(int count, Deck deck) {
+        for (int i = 0; i < count; i++) {
+            cards.add(deck.dealCard());
         }
-        else {
-            deck.addCards(noOfCards, mainDeck);
+    }
+
+    public Card playCard(char leadSuit) {
+        List<Card> playableCards = new ArrayList<Card>();
+        for (Card card : deck.getCards()) {
+            if (leadCard == null || card.getSuit() == leadSuit) {
+                playableCards.add(card);
+            }
         }
-        
+        if (playableCards.isEmpty()) {
+            // implement logic to play any card if player
+            // doesn't have any playable cards in their hand
+        }
+        // implement logic to choose which playable card
+        // to play based on strategy
+        Card playedCard = playableCards.get(0);
+        deck.removeCard(playedCard);
+        return playedCard;
+    }
+
+    public void playCard(Card card, Deck centerDeck) {
+        this.cards.remove(card);
+        centerDeck.addCard(card);
+    }
+
+    public List<Card> getHand() {
+        return deck.getCards();
+    }
+
+    public Card getLeadCard() {
+        return leadCard;
+    }
+
+    public void setLeadCard(Card leadCard) {
+        this.leadCard = leadCard;
     }
 
     @Override
     public String toString() {
-        return "Player" + (number+1) + ": " + deck;
+        return "Player " + (this.number + 1) + " (Score: " + this.score + ")";
     } 
 }
